d8cae672f32f71be3acf1057fa0c7e8abf29187a
Ankit Kumar <itskumarankit52@gmail.com>
2025-12-10 22:57:14 +0530

    utils code discussion

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse{
+    constructor(statusCode, data, messagae= "Success"){
+        this.statusCode= statusCode;
+        this.data= data;
+        this.message= messagae; 
+        this.success= statusCode < 400;
+    }
+}
